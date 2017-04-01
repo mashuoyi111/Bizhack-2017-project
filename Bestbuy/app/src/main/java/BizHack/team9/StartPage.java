0b58382cf2ce6d965ea1da30ad76dbbb0f0a571b
susanzc <susanszc@gmail.com>
2017-03-31 22:08:34 -0700

    startpage edit

--- Bestbuy/app/src/main/java/BizHack/team9/StartPage.java
+++ Bestbuy/app/src/main/java/BizHack/team9/StartPage.java
@@ -3,6 +3,8 @@ package BizHack.team9;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartPage extends Activity {
 
@@ -10,5 +12,20 @@ public class StartPage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+
+        final Button button = (Button) findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+
+
+        }
     }
+}
+
+
+
+
+    
 }

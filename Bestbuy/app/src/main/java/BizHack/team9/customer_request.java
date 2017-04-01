d023056ce59b146422809f1045b6993abaae2bfa
mashuoyi111 <mashuoyi111@sina.com>
2017-03-31 21:49:48 -0700

    delete location, add http client

--- Bestbuy/app/src/main/java/BizHack/team9/customer_request.java
+++ Bestbuy/app/src/main/java/BizHack/team9/customer_request.java
@@ -1,8 +1,18 @@
 package BizHack.team9;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Ma on 2017/3/31.
  */
 
-public class customer_request {
+
+public class customer_request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_request);
+    }
 }
--- Bestbuy/app/src/main/java/BizHack/team9/employee_request.java
+++ Bestbuy/app/src/main/java/BizHack/team9/employee_request.java
@@ -1,8 +1,18 @@
 package BizHack.team9;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Ma on 2017/3/31.
  */
 
-public class employee_request {
+
+public class employee_request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_request);
+    }
 }
